635afdd2c319b01e6961de6b25ca51c60b98524b
zhixin9001 <zhixin9001@126.com>
2017-06-13 20:21:26 +0800

    test upload

---
+++ Fit/Fit.AdminWeb/Controllers/UploadTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fit.AdminWeb.Controllers
+{
+  public class UploadTestController : Controller
+  {
+    [HttpGet]
+    public ActionResult Upload()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult UploadP()
+    {
+      return View();
+    }
+  }
+}
